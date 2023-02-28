465ebae445bc24abac5646fcdb6844fc39cfd834
Yevhenii Nabok <1337niceshot@gmail.com>
2023-02-28 18:11:13 +0200

    connect database

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,9 +1,17 @@
-using Microsoft.AspNetCore.ResponseCompression;
+using BlazorShop.Server.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
