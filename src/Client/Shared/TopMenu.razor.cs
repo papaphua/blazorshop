1e31ce127a31caac999252c3f392872209cd3800
Yevhenii Nabok <1337niceshot@gmail.com>
2023-03-16 23:11:45 +0200

    fix previous updates

---
+++ src/Client/Shared/TopMenu.razor.cs
@@ -0,0 +1,9 @@
+﻿using BlazorShop.Client.Services.AuthService;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorShop.Client.Shared;
+
+public partial class TopMenu
+{
+    [Inject] private IAuthService AuthService { get; set; } = null!;
+}
