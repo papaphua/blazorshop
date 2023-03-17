a3ce6fbd1b2473a7a0af381f9ef416501e18407f
Yevhenii Nabok <1337niceshot@gmail.com>
2023-03-17 23:27:42 +0200

    update all pages

--- src/Client/Shared/SideMenu.razor.cs
+++ src/Client/Shared/SideMenu.razor.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorShop.Client.Services.CategoryService;
+using BlazorShop.Client.Services.ProfileService;
 using BlazorShop.Shared.Dtos;
 using Microsoft.AspNetCore.Components;
 
@@ -7,6 +8,7 @@ namespace BlazorShop.Client.Shared;
 public sealed partial class SideMenu
 {
     [Inject] private ICategoryService CategoryService { get; set; } = null!;
+    [Inject] private IProfileService ProfileService { get; set; } = null!;
 
     private List<CategoryDto> Categories { get; set; } = new();
 
--- src/Shared/Models/CartItem.cs
+++ src/Shared/Models/CartItem.cs
@@ -8,12 +8,9 @@ public class CartItem
     public CartItem(ProductDto product)
     {
         Product = product;
-        TotalPrice = product.Price;
     }
     
     [Required] public ProductDto Product { get; }
 
     [Required] public int Quantity { get; set; } = 1;
-    
-    [Required] public decimal TotalPrice { get; set; }
 }
